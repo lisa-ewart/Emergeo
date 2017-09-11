6e1031ef905f49b798a2c5dc839d739accae2130
lisa-ewart <lnemedium@gmail.com>
2017-09-11 16:39:31 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import SignIn from './js/components/signin'
+import Profile from './js/components/profile'
+import MyScene from './js/components/MyScene'
 
 export default class App extends React.Component {
   render() {
-    return (
+    return (   
       <View style={styles.container}>
-        <Text>Emergeo Main screen!</Text>
-        
+        <Text style={styles.heading}>Emergeo!</Text>
+        <SignIn />
       </View>
     );
   }
@@ -18,5 +21,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
   },
+  heading:{
+    color:'#ccc',
+    fontWeight: 'bold',
+    fontSize:25,
+  }
 });
